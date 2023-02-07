3d41f7a29081524b97f09949119f73e30251236e
Marten Schuitemaker <marten.schuitemaker@etu.univ-nantes.fr>
2023-02-07 11:22:00 +0100

    ajout de routes sur API (autocomplete et recherche par animal)

--- herisson/api/api.js
+++ herisson/api/api.js
@@ -2,6 +2,7 @@
 
 const app = require( 'express' )();
 const path = require('path');
+const fs = require("fs");
 
 // Sample endpoint that sends the partner's name
 app.get('/topic', function ( req, res ) {
@@ -12,6 +13,38 @@ app.get('/topic', function ( req, res ) {
     // Send it as a JSON object
     res.json({'topic':topic});
 } );
+
+app.get('/animal/autocomplete/:name', function ( req, res ) {
+    const database = JSON.parse(fs.readFileSync('herisson/public/data/additionalDB.json').toString());
+    const filteredNames = Object.keys(database)
+        .filter(key => key.toLowerCase().startsWith(req.params.name.toLowerCase()));
+    console.log(filteredNames);
+    res.json({filteredNames});
+});
+
+app.get('/animal/:name', function ( req, res ) {
+    const database = JSON.parse(fs.readFileSync('herisson/public/data/additionalDB.json').toString());
+    const filteredData = Object.keys(database)
+        .filter(key => key.toLowerCase().startsWith(req.params.name.toLowerCase()))
+        .reduce((filteredData, key) => {
+            filteredData[key] = database[key];
+            return filteredData;
+        }, {});
+
+    res.json({filteredData});
+});
+
+app.get('/commune/autocomplete/:name', function ( req, res ) {
+    const database = JSON.parse(fs.readFileSync('herisson/public/data/db.json').toString());
+    const filteredData = Object.keys(database)
+        .filter(key => key.toLowerCase().startsWith(req.params.name.toLowerCase()))
+        .reduce((filteredData, key) => {
+            filteredData[key] = database[key];
+            return filteredData;
+        }, {});
+
+    res.json({filteredData});
+});
 
 // Export our API
 module.exports = app;
